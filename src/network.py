a4001969bb691342fa2a155b87d9b68a2c3b1210
teddy92729 <teddy92729@gmail.com>
2024-05-01 17:46:15 +0800

    註解

--- src/network.py
+++ src/network.py
@@ -8,38 +8,37 @@ class packet:
     pass
 
 
-class host_port:
-    pass
-
-
-class network:
-    pass
-
-
 class packet:
+    # static variable to get total number of packets
     packet_num: int = 0
 
     def __init__(self, packet_data: tuple[int, float] | None = None):
         self.packet_num: int
         self.time: float
+        # if packet_data is not None parse it
         if packet_data is not None:
             self.packet_num = packet_data[0]
             self.time = packet_data[1]
         else:
+            # if packet_data is None then set new packet_num and reset time
             packet.packet_num += 1
             self.packet_num = packet.packet_num
             self.time = -1
 
     def __str__(self) -> str:
+        # define string representation of packet
         return f"Packet {self.packet_num} sended at t = {self.time:.5f}"
 
     @staticmethod
     def encode(pkt: packet) -> bytes:
+        # record time and encode packet to bytes
         pkt.time = time.time()
         return str(pkt).encode()
 
     @staticmethod
     def decode(data: bytes) -> packet | None:
+        # decode packet from bytes and try to parse with regex
+        # if parsing fails return None
         data = data.decode()
         data = re.search(r"^Packet (\d+) sended at t = (\d+.\d+)$", data).groups()
         try:
@@ -61,6 +60,7 @@ class network:
         receiver: host_port = host_port("127.0.0.1", 5405),
         timeout: int = 10,
     ):
+        # network has dual socket for sending and receiving packets
         self.sender: host_port = sender
         self.receiver: host_port = receiver
         self.ssock: socket = socket(AF_INET, SOCK_DGRAM)
@@ -69,9 +69,12 @@ class network:
         self.rsock.settimeout(timeout)
 
     def sendto(self, pkts: list[packet,]) -> list[int]:
+        # threading lock to prevent competition of rpkts
         lock = threading.Lock()
+        # received packets
         rpkts: list[packet,] = []
 
+        # function to recive packet
         def recive_packet():
             try:
                 data, _ = self.rsock.recvfrom(65535)
@@ -83,6 +86,7 @@ class network:
             except:
                 pass
 
+        # function to send packets
         def send_packets():
             for pkt in pkts:
                 data = packet.encode(pkt)
@@ -95,12 +99,14 @@ class network:
             thread.start()
         for thread in threads:
             thread.join()
-        return [pkt.packet_num for pkt in rpkts]
+        return rpkts
 
 
 if __name__ == "__main__":
-    import random
+    from random import randint
 
-    pkts = [packet() for _ in range(random.randint(1, 10))]
+    pkts = [packet() for _ in range(randint(5, 10))]
     net = network()
-    print(net.sendto(pkts))
+    print(*net.sendto(pkts), sep="\n")
+    print("-" * 50)
+    print(*pkts, sep="\n")
