899b45bcb8fe5d08df75b8b15596bf39f4f5cd84
teddy92729 <teddy92729@gmail.com>
2024-05-14 02:59:29 +0000

    將快速重傳改為常數（原本定義為剩餘視窗的一半）

--- src/client.py
+++ src/client.py
@@ -4,27 +4,34 @@ from time import time
 import logging
 
 
-def client(source: Address, target: Address, pkts_nums: int, timeout: int = 2) -> None:
-    pkts = [Packet() for _ in range(pkts_nums)]
+def client(
+    source: Address,
+    target: Address,
+    pkts_num: int,
+    timeout: int = 2,
+    fast_retransmit: int = 3,
+    window: int = 16,
+    **kwargs,
+) -> None:
+    pkts = [Packet() for _ in range(pkts_num)]
     with socket(AF_INET, SOCK_DGRAM) as skt:
         skt.bind(tuple(source))
         skt.settimeout(timeout)
         logging.info(f"Client started at {source}")
 
-        # window size
-        window = 16
-
+        batch = 0
         total_sent = 0
 
         timer = Timer()
         # index of first packet in this batch
         index = 0
         while index < len(pkts):
+            batch += 1
             # prevent out of bounds
             bound_window = min(window, len(pkts) - index)
             # get packets that will be sent
             ready_pkts = pkts[index : index + bound_window]
-            logging.debug("*" * 50)
+            logging.debug(f"batch {batch}" + "*" * 50)
             for pkt in ready_pkts:
                 total_sent += 1
                 skt.sendto(Packet.encode(pkt), tuple(target))
@@ -49,30 +56,31 @@ def client(source: Address, target: Address, pkts_nums: int, timeout: int = 2) -
                     # all packets are received
                     if offset == bound_window - 1:
                         index += bound_window
-                        logging.debug("Normally window move")
+                        logging.debug(
+                            f"Window move {bound_window}: all packets received"
+                        )
                         break
 
                     if last_pkt and last_pkt == pkt:
                         counter += 1
                     else:
-                        counter = 0
+                        counter = 1
 
                     # when we receive same ack packets
-                    # and the number of same ack packets is greater than half of remaining window size
                     # we can move window to the next packet earlier
-                    if counter == max((window - offset) // 2, 2):
+                    if fast_retransmit and counter >= fast_retransmit:
                         index += offset + 1
-                        logging.debug("Early window move")
+                        logging.debug(f"Window move {offset + 1}: fast retransmit")
                         break
             except TimeoutError:
                 if pkt and offset >= 0:  # check last ack packet
                     index += offset + 1
-                logging.debug("Timeout")
+                logging.debug(f"Window move {offset + 1}: timeout")
         t = timer()
         logging.info(f"Total time: {t:.5f} sec")
-        logging.info(f"rtt: {t/pkts_nums:.5f} sec")
+        logging.info(f"rtt: {t/pkts_num:.5f} sec")
         logging.info(
-            f"Packets resent rate: {(total_sent - pkts_nums) / pkts_nums * 100:.5f} %"
+            f"Packets resent rate: {(total_sent - pkts_num) / pkts_num * 100:.5f} %"
         )
 
 
@@ -90,6 +98,13 @@ if __name__ == "__main__":
         "--pkts_num", type=int, default=1000, help="Number of packets to send"
     )
     parser.add_argument("--timeout", type=int, default=2, help="Timeout")
+    parser.add_argument(
+        "--fast_retransmit",
+        type=int,
+        default=3,
+        help="Fast retransmit (the number of same ack packets; 0 to disable)",
+    )
+    parser.add_argument("--window", type=int, default=16, help="Window size")
     parser.add_argument("--verbose", action="store_true", help="Verbose mode")
     parser.add_argument("--log", type=str, default="client.log", help="Log file")
     args = parser.parse_args()
@@ -109,4 +124,4 @@ if __name__ == "__main__":
             format="%(asctime)s - %(message)s",
         )
 
-    client(args.source, args.target, args.pkts_num, args.timeout)
+    client(**vars(args))
